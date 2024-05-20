743bd3f0fac91c4a449e8c64d0e5e27ffb78cfd1
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-05-20 18:41:54 +0200

    Create 1.5.1CopyingFilesExcercise1.6.c

---
+++ 1.5.1CopyingFilesExcercise1.6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+    
+    if(getchar() != EOF)
+    {
+        printf("getchar() != EOF have value 1");
+    }else{
+        printf("getchar() != EOF have value 0");
+    }
+    
+}
