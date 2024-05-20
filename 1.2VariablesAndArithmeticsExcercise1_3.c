f6fbc6a3df47b20a68fadb247c924caeda1aee12
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-05-20 20:44:58 +0200

    Update 1.2VariablesAndArithmeticsExcercise1_3.c

--- 1.2VariablesAndArithmeticsExcercise1_3.c
+++ 1.2VariablesAndArithmeticsExcercise1_3.c
@@ -8,10 +8,10 @@ void main(){
   step=20;
 
   fahr= lower;
-  printf("Fahrehait\t Celsius\n");
+  printf("Fahrehait\t Celsius\n"); //header in exercise 
   while(fahr<=upper){
     celsius = (5.0/9.0) * (fahr-32);
-    printf("%3.0f \t\t%6.1f\n",fahr,celsius);
+    printf("%3.0f \t\t%6.1f\n",fahr,celsius);// 2 tabs was usedy to align output
     fahr=fahr+step;
   }
 }
